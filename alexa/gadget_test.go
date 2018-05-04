008a18d9d5d6a3ee5ec5041bb295d170961fa713
patst <patrick.steinig@googlemail.com>
2018-05-04 08:16:08 +0200

    added testcase

--- alexa/gadget_test.go
+++ alexa/gadget_test.go
@@ -1,6 +1,7 @@
 package alexa
 
 import (
+	"encoding/json"
 	"testing"
 
 	hex "github.com/dlion/hex2rgb"
@@ -15,4 +16,29 @@ func TestRGBToHex(t *testing.T) {
 		hex := RgbToHex(r, g, b)
 		assert.Equal(t, values[i], hex)
 	}
+}
+
+func TestAddSetLightDirective(t *testing.T) {
+	var response Response
+	gadgetAnimation := GadgetAnimation{
+		Repeat:       1,
+		TargetLights: []string{"myTarget"},
+		Sequence: []GadgetAnimationStep{
+			GadgetAnimationStep{
+				Blend:      true,
+				Color:      "red",
+				DurationMs: 1000,
+			},
+		},
+	}
+	directive := response.AddGadgetControllerSetLightDirective([]string{"target"}, "none", 100, []GadgetAnimation{
+		gadgetAnimation,
+	})
+
+	bytes, err := json.Marshal(directive)
+	assert.NoError(t, err)
+	var unmarshalledDirective GadgetControllerSetLightDirective
+	json.Unmarshal(bytes, &unmarshalledDirective)
+
+	assert.Equal(t, directive, &unmarshalledDirective)
 }
